e06687e5c1fa500b9e65860f9a9b2d2d4857716c
alopine <loariel98@gmail.com>
2022-04-02 16:50:55 -0700

    Create playRound function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -13,4 +13,39 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            return "Tie!";
+        } else if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You Win! Paper beats Rock";
+        } else if (computerSelection === "paper") {
+            return "Tie!";
+        } else if (computerSelection === "scissors") {
+            return "You Lose! Scissors beat Paper";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            return "You Win! Scissors beat Paper";
+        } else if (computerSelection === "scissors") {
+            return "Tie!";
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
 console.log("Computer chooses: ", computerPlay());
+
+console.log(playRound(playerSelection, computerSelection));
