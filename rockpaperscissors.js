96d79080bd638414a100d5a40c1ce2326e47ebb7
alopine <loariel98@gmail.com>
2022-04-05 11:03:08 -0700

    Add CSS, restructure HTML, add restart and round counting functionality

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,9 +1,14 @@
-// Initialize scores
+// Initialize scores and round
 let playerScore = 0;
 let computerScore = 0;
+let roundCounter = 0;
 
-// Select buttons on page for player selection
+// Select round counter h2 on page
+const currentRound = document.querySelector("#round");
+
+// Select play buttons on page for player selection
 const btn = document.querySelectorAll(".btn");
+
 // Add event listeners
 btn.forEach((button) => {
     button.addEventListener("click", () => {
@@ -17,6 +22,15 @@ const results = document.querySelector("#results");
 // Select divs on page for scores
 const playerScoreText = document.querySelector("#playerScore");
 const computerScoreText = document.querySelector("#computerScore");
+
+// Select restart button on page
+const restart = document.querySelector("#restart");
+
+// Add event listener
+restart.addEventListener("click", () => {
+    restartGame();
+});
+
 
 function computerPlay() {
     // Create variable and assign to randomly generated number from 0 to 2
@@ -35,35 +49,77 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    if (playerScore === 5 || computerScore === 5) {
-        if (playerScore > computerScore) {
-            results.textContent = "Congratulations, you win!";
-        } else {
-            results.textContent = "Sorry, you lose!";
-        }
-    } else {
-        switch (true) {
-            // Tie
-            case (playerSelection === computerSelection):
-                results.textContent = "Tie!";
-                break;
-            // Player wins
-            case (playerSelection === "ROCK" && computerSelection === "SCISSORS"):
-            case (playerSelection === "PAPER" && computerSelection === "ROCK"):
-            case (playerSelection === "SCISSORS" && computerSelection === "PAPER"):
-                results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-                playerScore++;
-                playerScoreText.textContent = `Player: ${playerScore}`;
-                break;
-            // Player loses
-            case (computerSelection === "ROCK" && playerSelection === "SCISSORS"):
-            case (computerSelection === "PAPER" && playerSelection === "ROCK"):
-            case (computerSelection === "SCISSORS" && playerSelection === "PAPER"):
-                results.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
-                computerScore++;
-                computerScoreText.textContent = `Computer: ${computerScore}`;
-                break;
-        }
+    let roundResult = 0;
+
+    switch (true) {
+        // Game has been won
+        case (playerScore === 5 || computerScore === 5):
+            break;
+        // Tie
+        case (playerSelection === computerSelection):
+            roundCounter++;
+            break;
+        // Player wins round
+        case (playerSelection === "ROCK" && computerSelection === "SCISSORS"):
+        case (playerSelection === "PAPER" && computerSelection === "ROCK"):
+        case (playerSelection === "SCISSORS" && computerSelection === "PAPER"):
+            roundCounter++;
+            playerScore++;
+            roundResult = 1;
+            break;
+        // Computer wins round
+        case (computerSelection === "ROCK" && playerSelection === "SCISSORS"):
+        case (computerSelection === "PAPER" && playerSelection === "ROCK"):
+        case (computerSelection === "SCISSORS" && playerSelection === "PAPER"):
+            roundCounter++;
+            computerScore++;
+            roundResult = 2;
+            break;
     }
+
+    currentRound.textContent = `Round ${roundCounter}`;
+
+    displayResults(roundResult, playerSelection, computerSelection);
+}
+
+
+function displayResults(result, playerSelection, computerSelection) {
+
+    switch(true) {
+        // Player wins game
+        case (playerScore === 5):
+            results.textContent = "Congratulations, you win!";
+            playerScoreText.textContent = `Player: ${playerScore}`;
+            break;
+        // Computer wins game
+        case (computerScore === 5):
+            results.textContent = "Sorry, you lose!";
+            computerScoreText.textContent = `Computer: ${computerScore}`;
+            break;
+        // Player wins round
+        case (result === 1):
+            results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+            playerScoreText.textContent = `Player: ${playerScore}`;
+            break;
+        // Computer wins round
+        case (result === 2):
+            results.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            computerScoreText.textContent = `Computer: ${computerScore}`;
+            break;
+        // Tie
+        case (result === 0):
+            results.textContent = "Tie!";
+    }
+}
+
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    roundCounter = 0;
+
+    currentRound.textContent = `Round ${roundCounter}`;
+    playerScoreText.textContent = `Player: ${playerScore}`;
+    computerScoreText.textContent = `Computer: ${computerScore}`;
+    results.textContent = "";
 }
